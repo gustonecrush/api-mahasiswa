64b49bd2c4d6ac1f61d1585bd10c370a62d2875e
gustonecrush <codeboxezz@gmail.com>
2023-01-06 18:21:39 +0700

    created methods in controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,30 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function sendResponse($result, $message, $code) {
+        $response = [
+            "success"     => true,
+            "status_code" => $code,
+            "message"     => $message,
+            "data"        => $result
+        ];
+
+        return response()->json($response, $code);
+    }
+
+    protected function sendError($error, $errorMessages = [], $code) {
+        $response = [
+            "success"     => false,
+            "status_code" => $code,
+            "message"     => $error
+        ];
+
+        if (!empty($errorMessages)) {
+            $response["error"] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+
 }
